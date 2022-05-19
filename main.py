42bf6af4de3592aa7ac3d83d2a8703db7336ff69
DesertEaglefff <105866434+DesertEaglefff@users.noreply.github.com>
2022-05-19 17:09:30 +0300

    Update main.py
    этот комит создан для проверки редактировании на удалённом сервере

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('And I cange it every day')
     print('And I cange it every day')
-
+    print('volk')
 
 if _name_ == '_main_':
     main()
